c52fd255eec9cb185de762ce2fc469d06d5a5e97
Allan <workofcode@gmail.com>
2022-01-23 17:17:44 +0000

    update: List
    - Small cleanup on the List & Default JSON, trying to see if I actually have free time to work on this

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,27 +1,32 @@
 <template>
-  <div class="d-flex justify-content-center pre-loader" v-if="loading === true || loadingError === true">
-    <div class="spinner-border" role="status" v-if="!loadingError">
-      <span class="sr-only"></span>
-    </div>
-    <p v-if="!loadingError">Now Loading, please wait...</p>
-    <p v-else>There was an error loading this data</p>
-    <router-link to="/">Return Home</router-link>
-  </div>
-
   <div class="introduction-banner left">
     <div class="container">
       <div class="row">
         <div class="col">
           <h1>List of Aircraft:</h1>
-          <p>Below you can find the list of Aircraft we have checklists for and those we are working on!<br>
-          You can always <router-link to="contribute">contribute</router-link> your own checklists to FlightLists!</p>
+          <p>
+            Below you can find the list of Aircraft we have checklists for and those we are working on!
+            <br />You can always
+            <router-link to="contribute">contribute</router-link>your own checklists to FlightLists!
+          </p>
         </div>
       </div>
     </div>
   </div>
+  {{ ListData }}
+  <div
+    class="d-flex justify-content-center pre-loader"
+    v-if="loading === true || ListData.length == 0"
+  >
+    <div class="spinner-border" role="status" v-if="loading">
+      <span class="sr-only"></span>
+    </div>
+    <p v-if="loading">Now Loading, please wait...</p>
+    <p v-else>There was an error loading this data</p>
+    <router-link to="/">Return Home</router-link>
+  </div>
 
-
-  <div class="list-container" v-if="!loading && !loadingError">
+  <div class="list-container" v-if="!loading && ListData.length > 0">
     <div class="container">
       <div class="row">
         <div class="col">
@@ -36,22 +41,15 @@
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="item in ListData" :key="item.ID"
-                    @click="viewList(item.Filename)"
-                >
-                  <th scope="row" class="align-middle">
-                    {{item.ID}}
-                  </th>
+                <tr v-for="item in ListData" :key="item.ID" @click="viewList(item.Filename)">
+                  <th scope="row" class="align-middle">{{ item.ID }}</th>
+                  <td class="align-middle">{{ item.Name }}</td>
+                  <td class="align-middle">{{ item.Status }}</td>
                   <td class="align-middle">
-                    {{item.Name}}
-                  </td>
-                  <td class="align-middle">
-                    {{item.Status}}
-                  </td>
-                  <td class="align-middle">
-                    <router-link class="btn btn-outline-secondary" :to="'/Checklist/' + item.Filename">
-                      View
-                    </router-link>
+                    <router-link
+                      class="btn btn-outline-secondary"
+                      :to="'/Checklist/' + item.Filename"
+                    >View</router-link>
                   </td>
                 </tr>
               </tbody>
@@ -63,16 +61,17 @@
   </div>
 </template>
 
-<script>
+<script type="ts">
 import axios from "axios";
+import { defineComponent } from "vue";
 
-export default {
+export default defineComponent({
   name: 'List',
-  data: function(){
+  data: function () {
     return {
-      loading: true, 
+      loading: true,
       loadingError: false,
-      ListData: {}
+      ListData: []
     }
   },
   mounted() {
@@ -81,29 +80,27 @@ export default {
       .get("/data/list.json")
       .then(response => {
         this.loading = false
-        this.ListData = response.data.Data
+        this.ListData = response.data
       }).catch(error => {
+        this.loading = false
         console.log('Error with loading data is: ' + error)
-        this.loadingError = true
       })
   }
-};
+});
 </script>
 
 <style lang="scss" scoped>
-    @import "../assets/vars.scss";
-    @import "../assets/common.scss";
+@import "../assets/vars.scss";
+@import "../assets/common.scss";
 
+.list-container {
+  .info {
+    background: #fff;
+    text-align: left;
+  }
 
-    .list-container {
-      .info {
-        background: #fff;
-        text-align: left;
-      }
-
-      table {
-        text-align: left;
-      }
-    }
-
+  table {
+    text-align: left;
+  }
+}
 </style>
