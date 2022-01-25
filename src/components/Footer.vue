e08e6ab570862cc28b81f7bef6721b6c0ef9ccfc
Allan <workofcode@gmail.com>
2022-01-25 18:05:07 +0000

    Cleanup
    - create footer / preloader component

---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <footer class="footer-main">
+    <p>
+      Created by:
+      <a href="https://www.allancodes.com" alt="AllanCodes" target="_blank">AllanCodes</a>,
+    </p>
+    <p>
+      Contribute at
+      <a href="https://www.github.com/AllanOcelot/FlightLists">GitHub</a>,
+    </p>
+    <p>
+      Please be sure to read our
+      <router-link to="/content-policy">Content Policy</router-link>.
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../assets/vars.scss";
+
+.footer-main {
+  border-top: 2px solid $brand-yellow;
+  background: $color-white;
+  padding: 10px 20px;
+  text-align: right;
+  font-size: 14px;
+  p {
+    margin: 0;
+    display: inline-block;
+    margin-right: 5px;
+    a {
+      color: $brand-blue;
+      text-decoration: none;
+    }
+  }
+}
+</style>
---
+++ src/components/PreLoader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="d-flex justify-content-center pre-loader" v-if="loading || error">
+    <div class="spinner-border" role="status" v-if="loading && !error">
+      <span class="sr-only"></span>
+    </div>
+    <p v-if="loading && !error">Now Loading, please wait...</p>
+    <p v-else>There was an error loading this data</p>
+    <router-link to="/">Return Home</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    loading: Boolean,
+    error: Boolean
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
